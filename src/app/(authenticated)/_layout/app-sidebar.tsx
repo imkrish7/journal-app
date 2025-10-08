762a72d33f3887c8ea4b7b2478a2085025774163
Rackham <singhkrishk07@gmail.com>
2025-10-09 01:55:57 +0530

    basic setup is done

---
+++ src/app/(authenticated)/_layout/app-sidebar.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import * as React from "react"
+import { ArchiveX, File, Inbox, Send, Trash2 } from "lucide-react"
+
+import { NavUser } from "./nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  
+} from "@/components/ui/sidebar"
+import { logoFont } from "@/app/layout"
+
+
+// This is sample data
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+      isActive: true,
+    },
+    {
+      title: "Drafts",
+      url: "#",
+      icon: File,
+      isActive: false,
+    },
+    {
+      title: "Sent",
+      url: "#",
+      icon: Send,
+      isActive: false,
+    },
+    {
+      title: "Junk",
+      url: "#",
+      icon: ArchiveX,
+      isActive: false,
+    },
+    {
+      title: "Trash",
+      url: "#",
+      icon: Trash2,
+      isActive: false,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  // Note: I'm using state to show active item.
+  // IRL you should use the url/router.
+//   const [activeItem, setActiveItem] = React.useState(data.navMain[0])
+ 
+//   const { setOpen } = useSidebar()
+
+  return (
+      <Sidebar
+          collapsible="icon"
+          variant="sidebar"
+        className="w-[calc(var(--sidebar-width-icon)+1px)]! border-r"
+          {...props}
+    
+      >
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
+                <a href="#">
+                  <div className="text-sidebar-secondary flex aspect-square size-10 items-center justify-center rounded-lg">
+                    {/* <Command className="size-4" />
+                     */}
+                    <span className={`font-bold text-3xl ${logoFont.className}`}>j</span>
+                  </div>
+                  
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent className="px-1.5 md:px-0">
+              <SidebarMenu>
+                {data.navMain.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      tooltip={{
+                        children: item.title,
+                        hidden: false,
+                      }}
+                      onClick={()=>{}}
+                    //   isActive={activeItem?.title === item.title}
+                      className="px-2.5 md:px-2"
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser user={data.user} />
+        </SidebarFooter>
+      </Sidebar>
+  )
+}
---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,28 @@
+import { AppSidebar } from "./_layout/app-sidebar"
+
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+      <SidebarProvider style={{
+        '--sidebar-width': '3rem'
+    } as React.CSSProperties}>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-muted/50 aspect-video h-12 w-full rounded-lg"
+            />
+          ))}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
