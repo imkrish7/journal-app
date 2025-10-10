eaebc82b3ded1bb0328034a9e122fcc985f8c45d
Rackham <singhkrishk07@gmail.com>
2025-10-11 02:10:02 +0530

    small imporovements on chats

--- src/app/(authenticated)/_layout/app-sidebar.tsx
+++ src/app/(authenticated)/_layout/app-sidebar.tsx
@@ -1,115 +1,110 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { NavUser } from "./nav-user"
+import * as React from "react";
+import { NavUser } from "./nav-user";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  
-} from "@/components/ui/sidebar"
-import { logoFont } from "@/app/layout"
-import MemoryLottie from "@/components/icons/MemoryLottie"
-import TodoLottie from "@/components/icons/TodoLottie"
-import ReminderLottie from "@/components/icons/ReminderLottie"
-import TrashLottie from "@/components/icons/TrashLottie"
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { logoFont } from "@/app/layout";
+import MemoryLottie from "@/components/icons/MemoryLottie";
+import TodoLottie from "@/components/icons/TodoLottie";
+import ReminderLottie from "@/components/icons/ReminderLottie";
+import TrashLottie from "@/components/icons/TrashLottie";
 
-import Link from "next/link"
-
+import Link from "next/link";
 
 // This is sample data
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Todoist",
-      url: "/todo",
-      icon: TodoLottie,
-      isActive: true,
-    },
-    {
-      title: "Memory",
-      url: "/memory",
-      icon: MemoryLottie,
-      isActive: false,
-    },
-    {
-      title: "Reminders",
-      url: "/reminders",
-      icon: ReminderLottie,
-      isActive: false,
-    },
-    {
-      title: "Trash",
-      url: "/trash",
-      icon: TrashLottie,
-      isActive: false,
-    },
-    
-  ],
-}
+	user: {
+		name: "shadcn",
+		email: "m@example.com",
+		avatar: "/avatars/shadcn.jpg",
+	},
+	navMain: [
+		{
+			title: "Todoist",
+			url: "/todo",
+			icon: TodoLottie,
+			isActive: true,
+		},
+		{
+			title: "Memory",
+			url: "/memory",
+			icon: MemoryLottie,
+			isActive: false,
+		},
+		{
+			title: "Reminders",
+			url: "/reminders",
+			icon: ReminderLottie,
+			isActive: false,
+		},
+		{
+			title: "Trash",
+			url: "/trash",
+			icon: TrashLottie,
+			isActive: false,
+		},
+	],
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  
-
-  return (
-      <Sidebar
-          collapsible="icon"
-          variant="sidebar"
-        className="w-[calc(var(--sidebar-width-icon)+1px)]! border-r"
-          {...props}
-    
-      >
-        <SidebarHeader>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
-                <Link href="#">
-                  <div className="text-sidebar-secondary flex aspect-square size-10 items-center justify-center rounded-lg">
-                    <span className={`font-bold text-3xl ${logoFont.className}`}>j</span>
-                  </div>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup className="p-1">
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {data.navMain.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton
-                      tooltip={{
-                        children: item.title,
-                        hidden: false,
-                      }}
-                      className="size-12 hover:bg-gray-200 mt-1"
-                    >
-                      <Link className="h-full w-full" href={item.url}>
-                        <item.icon/>
-                      </Link>
-                      
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <NavUser user={data.user} />
-        </SidebarFooter>
-      </Sidebar>
-  )
+	return (
+		<Sidebar
+			collapsible="icon"
+			variant="sidebar"
+			className="w-[calc(var(--sidebar-width-icon)+1px)]! border-r"
+			{...props}
+		>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
+							<Link href="/dashboard">
+								<div className="text-sidebar-secondary flex aspect-square size-10 items-center justify-center rounded-lg">
+									<span className={`font-bold text-3xl ${logoFont.className}`}>
+										j
+									</span>
+								</div>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup className="p-1">
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{data.navMain.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton
+										tooltip={{
+											children: item.title,
+											hidden: false,
+										}}
+										className="size-12 hover:bg-gray-200 mt-1"
+									>
+										<Link className="h-full w-full" href={item.url}>
+											<item.icon />
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,20 +1,21 @@
-"use client"
-import ChatInterface from '@/components/ChatInterface'
-import Message from '@/components/Message'
-import React from 'react'
+"use client";
+import ChatInterface from "@/components/ChatInterface";
+import Message from "@/components/Message";
+import React from "react";
 
-const message = "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist."
+const message =
+	"So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.";
 
 const page = () => {
-  return (
-    <div className='flex flex-col'>
-      <div className='flex-1 gap-2 flex flex-col'>
-        <Message who='USER' message='Hello' userAvatarLink='' />
-        <Message who='AI' message={message} userAvatarLink='' />
-      </div>
-      <ChatInterface />
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col p-4">
+			<div className="flex-1 gap-2 flex flex-col">
+				<Message who="USER" message="Hello" userAvatarLink="" />
+				<Message who="AI" message={message} userAvatarLink="" />
+			</div>
+			<ChatInterface />
+		</div>
+	);
+};
 
-export default page
+export default page;
